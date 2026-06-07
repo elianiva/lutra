9101dd04d0eeae4500a8326d404c7283e17ba5bf
elianiva <git@elianiva.com>
2026-06-07 12:34:48 +0700

    chore: move to dev build instead of expo go

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,16 @@ const { withUniwindConfig } = require("uniwind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Reanimated 4.4 throws on eager load because worklets' top-level init()
+// reads globals that aren't set up until after the native module is ready.
+// Defer the worklets/reanimated import graph behind actual usage.
+// https://github.com/software-mansion/react-native-reanimated/issues/9445
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    inlineRequires: true,
+  },
+});
+
 module.exports = withUniwindConfig(config, {
   cssEntryFile: "./src/global.css",
 });
